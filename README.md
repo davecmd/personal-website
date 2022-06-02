10e20570f64bafefc947fc69a63dc9123f3461c1
David Tan <dave.tan@mail.utoronto.ca>
2022-06-02 00:15:48 -0400

    Added animated hamburger menu, fixed small visual alignments , made header sticky

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # Personal-Website
+TODO: Improve flare(animations,colour...etc), add featured work (cards), clean up css html
---
+++ scripts.js
@@ -0,0 +1,4 @@
+function toggleHamburgerMenu() {
+	var element = document.getElementById("hamburger-menu");
+	element.classList.toggle("is-active");
+  }
