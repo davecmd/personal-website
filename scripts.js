5ad73e825b5d4b3a383904e0fe47743e87b6626e
David Tan <dave.tan@mail.utoronto.ca>
2022-06-06 23:48:49 -0400

    made modals for featured work and added animation to navigation bar. Also fixed formatting and screen size responsiveness

--- scripts.js
+++ scripts.js
@@ -2,3 +2,7 @@ function toggleHamburgerMenu() {
 	var element = document.getElementById("hamburger-menu");
 	element.classList.toggle("is-active");
   }
+
+  $(document).ready(function() {
+    $("#MyModal").modal();
+  });	
