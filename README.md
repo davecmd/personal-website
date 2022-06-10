0c97a34a9facd4bcf37f696e92287705ed6bdf22
David Tan <dave.tan@mail.utoronto.ca>
2022-06-10 00:03:24 -0400

    Added cookies to save dark mode changes

--- README.md
+++ README.md
@@ -4,12 +4,13 @@ TODO:
 - Finish Cards (pop up a modal with a clickable link to the github if available)
 - Clean up css html
 - Cleanup file system
-- Dark Mode (button and animate changing theme, save choice for future with cookies or local storage)
 
 BUGS:
  - hypertext link for email animation glitches (maybe switch to using div?)
- - dropdown dividers showing up in larger screens
  - navigation bar jumps up in the half way point
  - fix in between screen size formats
+ - if dark mode is saved in cookies, animation on toggle theme button plays on load and email. Also can randomly flicker white on load
+ - Resume sometimes fails to load in properly
+ 
  
  
--- scripts.js
+++ scripts.js
@@ -1,15 +1,78 @@
-function toggleHamburgerMenu() {
-	var element = document.getElementById("hamburger-menu");
-	element.classList.toggle("is-active");
-  }
-
-  $(document).ready(function() {
-    $("#MyModal").modal();
-  });	
-
-  function toggleTheme() {
+document.addEventListener("DOMContentLoaded", function(event) { 
+  var theme = getCookie("theme");
+  if (theme == "dark") {
     var element = document.getElementById("toggle-theme");
-    document.body.classList.toggle("dark-theme");
+    document.body.classList.toggle("dark-theme");   
+    document.body.classList.add("notransition");   
+    document.getElementById("header").classList.add("notransition");  
+    var navLinks = document.getElementsByClassName("nav-link");
+    for(var i = 0; i < navLinks.length; i++)
+    {
+      navLinks[i].classList.add('notransition');
     }
-  
-  
+   // document.getElementById("email").classList.add("notransition");
+    document.getElementById("toggle-theme").checked = true;
+
+  } else {
+    document.getElementById("toggle-theme").checked = false;
+  }
+});
+
+function toggleHamburgerMenu() {
+  var element = document.getElementById("hamburger-menu");
+  element.classList.toggle("is-active");
+}
+
+$(document).ready(function () {
+  $("#MyModal").modal();
+});
+
+function toggleTheme() {
+  var theme = getCookie("theme");
+  document.body.classList.remove("notransition");
+  document.getElementById("header").classList.remove("notransition");  
+  var navLinks = document.getElementsByClassName("nav-link");
+    for(var i = 0; i < navLinks.length; i++)
+    {
+      navLinks[i].classList.remove('notransition');
+    }
+//    document.getElementById("email").classList.remove("notransition");
+
+
+  if (theme == "dark") {
+    setCookie('theme','light',7);
+    var elements = document.getElementById("toggle-theme");
+    document.body.classList.toggle("dark-theme");   
+  } else {
+    setCookie('theme','dark',7);
+    var element = document.getElementById("toggle-theme");
+    document.body.classList.toggle("dark-theme");   
+  }
+  console.log("THEME: "+ theme);
+}
+
+/* Cookie Functions */
+function setCookie(name, value, days) {
+  var expires = "";
+  if (days) {
+    var date = new Date();
+    date.setTime(date.getTime() + days * 24 * 60 * 60 * 1000);
+    expires = "; expires=" + date.toUTCString();
+  }
+  document.cookie = name + "=" + (value || "") + expires + "; path=/";
+}
+
+function getCookie(name) {
+  var nameEQ = name + "=";
+  var ca = document.cookie.split(";");
+  for (var i = 0; i < ca.length; i++) {
+    var c = ca[i];
+    while (c.charAt(0) == " ") c = c.substring(1, c.length);
+    if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length, c.length);
+  }
+  return null;
+}
+
+function eraseCookie(name) {
+  document.cookie = name + "=; Path=/; Expires=Thu, 01 Jan 1970 00:00:01 GMT;";
+}
